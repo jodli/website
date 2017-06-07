530198e5468c039d6e7ededf1fd734d28690f8cd
JB <jb@visualjerk.de>
2017-06-08 00:50:37 +0200

    Watch me!

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <storyblock>
+      <beginning></beginning>
+    </storyblock>
+    <storyblock>
+      <start-with-see></start-with-see>
+    </storyblock>
+  </div>
+</template>
+
+<script>
+import Storyblock from './Storyblock'
+import Beginning from './Beginning'
+import StartWithSee from './StartWithSee'
+
+export default {
+  name: 'home',
+  data () {
+    return {}
+  },
+  components: {
+    Storyblock,
+    Beginning,
+    StartWithSee
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  perspective: 1px;
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+</style>
---
+++ src/components/Storyblock.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="storyblock">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'storyblock'
+}
+</script>
+
+<style lang="scss" scoped>
+.storyblock {
+  height: 100vh
+}
+</style>
